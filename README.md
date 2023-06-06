2554cdc9a04ea63f909545cd5641a3bf0cc7f230
Raul Barbosa <rbarbosa@dei.uc.pt>
2023-06-06 10:17:04 +0100

    cosmetic changes

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Petit
+
 The Petit programming language and compiler
+
+## Example
+
+    factorial(integer n) = if n then n * factorial(n-1) else 1
+
+## Grammar
+
+    program: IDENTIFIER '(' parameters ')' '=' expression
+           | program IDENTIFIER '(' parameters ')' '=' expression
+
+    parameters: parameter
+              | parameters ',' parameter
+
+    parameter: INTEGER IDENTIFIER
+             | DOUBLE IDENTIFIER
+
+    expression: IDENTIFIER
+              | NATURAL
+              | DECIMAL
+              | IDENTIFIER '(' arguments ')'
+              | IF expression THEN expression ELSE expression
+              | expression '+' expression
+              | expression '-' expression
+              | expression '*' expression
+              | expression '/' expression
+              | '(' expression ')'
+
+    arguments: expression
+              | arguments ',' expression
--- src/build.sh
+++ src/build.sh
@@ -7,29 +7,29 @@ gcc ast.c semantics.c codegen.c lex.yy.c y.tab.c -Wall -Wno-unused-function -o p
 
 # run syntax tests
 echo "\n[syntax tests]\n"
-./petit -t < ../test/factorial.pti
-./petit -t < ../test/fibonacci.pti
-./petit -t < ../test/fullgrammar.pti
+./petit -t < ../test/factorial.pt
+./petit -t < ../test/fibonacci.pt
+./petit -t < ../test/fullgrammar.pt
 
 # run semantics tests
 echo "\n[semantics tests]\n"
-./petit -s < ../test/semantic_errors.pti
-./petit -s < ../test/fibonacci.pti
-./petit -s < ../test/gcd.pti
-./petit -s < ../test/factorial.pti
-./petit -s < ../test/fullgrammar.pti
+./petit -s < ../test/semantic_errors.pt
+./petit -s < ../test/fibonacci.pt
+./petit -s < ../test/gcd.pt
+./petit -s < ../test/factorial.pt
+./petit -s < ../test/fullgrammar.pt
 
 # run code generation tests
 echo "\n[code generation tests]\n"
-./petit < ../test/gcd.pti
-./petit < ../test/factorial.pti
+./petit < ../test/gcd.pt
+./petit < ../test/factorial.pt
 
 # generate code, compile it, and run it
-./petit < ../test/gcd.pti > ../test/gcd.ll 
+./petit < ../test/gcd.pt > ../test/gcd.ll 
 llc ../test/gcd.ll -o ../test/gcd.s
 clang ../test/gcd.s io.c -o ../test/gcd
 ../test/gcd
-./petit < ../test/factorial.pti > ../test/factorial.ll 
+./petit < ../test/factorial.pt > ../test/factorial.ll 
 llc ../test/factorial.ll -o ../test/factorial.s
 clang ../test/factorial.s io.c -o ../test/factorial
 ../test/factorial
