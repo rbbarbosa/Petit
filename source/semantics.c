1e9b7bb33988ac8d63e9dea343fbdf4619c1600e
Raul Barbosa <rbarbosa@dei.uc.pt>
2025-01-23 14:37:37 +0000

    more readable insert_symbol to close #2

--- source/semantics.c
+++ source/semantics.c
@@ -104,22 +104,17 @@ int check_program(struct node *program) {
 
 // insert a new symbol in the list, unless it is already there
 struct symbol_list *insert_symbol(struct symbol_list *table, char *identifier, enum type type, struct node *node) {
+    if(search_symbol(table, identifier) != NULL)
+        return NULL;       /* return NULL if symbol is already inserted */
     struct symbol_list *new = (struct symbol_list *) malloc(sizeof(struct symbol_list));
     new->identifier = strdup(identifier);
     new->type = type;
     new->node = node;
     new->next = NULL;
     struct symbol_list *symbol = table;
-    while(symbol != NULL) {
-        if(symbol->next == NULL) {
-            symbol->next = new;    /* insert new symbol at the tail of the list */
-            break;
-        } else if(strcmp(symbol->next->identifier, identifier) == 0) {
-            free(new);
-            return NULL;           /* return NULL if symbol is already inserted */
-        }
+    while(symbol->next != NULL)
         symbol = symbol->next;
-    }
+    symbol->next = new;    /* insert new symbol at the tail of the list */
     return new;
 }
 
